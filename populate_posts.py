687fe41c2bdda97fa30145e72066638aded20c32
jackalnom <jackalnom@gmail.com>
2023-05-24 09:28:24 -0700

    fixed up posts

--- populate_posts.py
+++ populate_posts.py
@@ -15,7 +15,7 @@ def database_connection_url():
 
 # Create a new DB engine based on our connection string
 engine = sqlalchemy.create_engine(database_connection_url(), use_insertmanyvalues=True)
-categories = ['News', 'Sports', 'Politics', 'Entertainment']
+categories = ['News', 'Sports', 'Politics', 'Entertainment', 'Technology', 'Science', 'Health', 'Business', 'Travel', 'Food']
 
 with engine.begin() as conn:
     conn.execute(sqlalchemy.text("""
@@ -33,9 +33,10 @@ with engine.begin() as conn:
     CREATE TABLE
     users (
         id int generated always as identity not null PRIMARY KEY,
-        username text not null,
+        username text unique not null,
         full_name text not null,
-        birthday date not null
+        birthday date not null,
+        device_type text not null
     );    
         
     CREATE TABLE
@@ -46,15 +47,9 @@ with engine.begin() as conn:
         created_at timestamp not null,
         visible boolean not null,
         poster_id int not null references users(id),
-        category_id int  not null references category(id)
-    );
-
-    CREATE TABLE
-    likes (
-        user_id int references users(id),
-        post_id int references posts(id),
-        PRIMARY KEY (user_id, post_id),
-        created_at timestamp not null
+        category_id int  not null references category(id),
+        likes int default 0,
+        nsfw boolean default false
     );
     """))
     
@@ -67,7 +62,9 @@ with engine.begin() as conn:
 num_users = 200000
 fake = Faker()
 posts_sample_distribution = np.random.default_rng().negative_binomial(0.04, 0.01, num_users)
-category_sample_distribution = np.random.choice([1, 2, 3, 4], num_users, p=[0.1, 0.3, 0.1, 0.5])
+category_sample_distribution = np.random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                                                 num_users,
+                                                p=[0.1, 0.05, 0.1, 0.3, 0.05, 0.05, 0.05, 0.05, 0.15, 0.1])
 total_posts = 0
 
 # create fake posters with fake names and birthdays
@@ -79,22 +76,26 @@ with engine.begin() as conn:
             print(i)
         
         profile = fake.profile()
+        username = fake.unique.email()
+        device_type = fake.random_element(elements=('Android', 'iOS', 'Web'))
 
         poster_id = conn.execute(sqlalchemy.text("""
-        INSERT INTO users (username, full_name, birthday) VALUES (:username, :name, :birthday) RETURNING id;
-        """), {"username": profile['username'], "name": profile['name'], "birthday": profile['birthdate']}).scalar_one();
+        INSERT INTO users (username, full_name, birthday, device_type) VALUES (:username, :name, :birthday, :device_type) RETURNING id;
+        """), {"username": username, "name": profile['name'], "birthday": profile['birthdate'], "device_type": device_type}).scalar_one();
 
         num_posts = posts_sample_distribution[i]
-
-        for _ in range(num_posts):
+        likes_sample_distribution = np.random.default_rng().negative_binomial(0.8, 0.0001, num_posts)  
+        for j in range(num_posts):
             total_posts += 1
             posts.append({
                 "title": fake.sentence(),
                 "content": fake.text(),
                 "poster_id": poster_id,
                 "category_id": category_sample_distribution[i].item(),
-                "visible": fake.boolean(97),
-                "created_at": fake.date_time_between(start_date='-5y', end_date='now', tzinfo=None)
+                "visible": fake.boolean(75),
+                "created_at": fake.date_time_between(start_date='-5y', end_date='now', tzinfo=None),
+                "likes": likes_sample_distribution[j].item(),
+                "nsfw": fake.boolean(10)
             })
 
     if posts:
@@ -103,34 +104,5 @@ with engine.begin() as conn:
         VALUES (:title, :content, :poster_id, :category_id, :visible, :created_at);
         """), posts)
 
-    max_post_id = conn.execute(sqlalchemy.text("""
-    SELECT MAX(id) FROM posts;
-    """
-    )).scalar_one()
-
     print("total posts: ", total_posts)
     
-    print("creating fake likes...")
-    # create fake likes
-    likes = []
-    for user_id in range(poster_id, poster_id + num_users):
-        if (user_id % 10 == 0):
-            print(user_id)
-
-        num_likes = min(total_posts, np.random.default_rng().negative_binomial(0.5, 0.001, 1).item())
-
-        if num_likes:
-            liked_post_id = np.random.default_rng().choice(max_post_id-1, size=num_likes, replace=False)
-
-            for i in range(num_likes):
-                likes.append({
-                    "user_id": user_id,
-                    "post_id": liked_post_id[i].item()+1,
-                    "created_at": fake.date_time_between(start_date='-5y', end_date='now', tzinfo=None)
-                })
-
-    if likes:
-        conn.execute(sqlalchemy.text("""
-        INSERT INTO likes (user_id, post_id, created_at) 
-        VALUES (:user_id, :post_id, :created_at);
-        """), likes)
